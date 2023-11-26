41419c2716f392c8c1e9fc9581e8358a61eb9b1e
Bárbara Flores <bpf17@duke.edu>
2023-11-25 20:41:21 -0500

    update

--- naive_bayes_classifier.py
+++ naive_bayes_classifier.py
@@ -1,82 +1,136 @@
-"""
-References
-This code is based on the original work by Patrick Wang in the NLP class.
-Utilizing code from classification_example.py.
-"""
+"""Compare token/document vectors for classification."""
+import random
+from typing import List, Mapping, Optional, Sequence
 
-from collections import Counter
-import math
-from typing import Optional, List
 import nltk
+import numpy as np
+from numpy.typing import NDArray
+from sklearn.linear_model import LogisticRegression
+
+FloatArray = NDArray[np.float64]
 
 
-def onehot(vocabulary: List[Optional[str]], token: Optional[str]) -> List[int]:
+def onehot(
+    vocabulary_map: Mapping[Optional[str], int], token: Optional[str]
+) -> FloatArray:
     """Generate the one-hot encoding for the provided token in the provided vocabulary."""
-    embedding = [0 for _ in range(len(vocabulary))]
-    try:
-        idx = vocabulary.index(token)
-    except ValueError:
-        idx = len(vocabulary) - 1
+    embedding = np.zeros((len(vocabulary_map),))
+    idx = vocabulary_map.get(token, len(vocabulary_map) - 1)
     embedding[idx] = 1
     return embedding
 
 
-# define model
-class UnigramModel:
-    """The unigram language model."""
-
-    def __init__(self) -> None:
-        """Initialize."""
-        self.p: Optional[List[float]] = None
-
-    def train(self, encodings: List[List[int]]) -> "UnigramModel":
-        """Train the model on data."""
-        counts = [1 for _ in range(len(vocabulary))]  # add-1 smoothing!!
-        for encoding in encodings:
-            for v in range(len(encoding)):
-                counts[v] += encoding[v]
-        self.p = [count / len(encodings) for count in counts]
-        return self
-
-    def apply(self, encodings: List[List[int]]) -> float:
-        """Compute the log probability of a document."""
-        if self.p is None:
-            raise ValueError("This model is untrained")
-        return sum(math.log(dot_product(encoding, self.p)) for encoding in encodings)
+def sum_token_embeddings(
+    token_embeddings: Sequence[FloatArray],
+) -> FloatArray:
+    """Sum the token embeddings."""
+    total: FloatArray = np.array(token_embeddings).sum(axis=0)
+    return total
 
 
-def dot_product(a: List[int], b: List[float]) -> float:
-    """Compute the dot product of vectors a and b."""
-    return sum(a_i * b_i for a_i, b_i in zip(a, b))
+def split_train_test(
+    X: FloatArray, y: FloatArray, test_percent: float = 10
+) -> tuple[FloatArray, FloatArray, FloatArray, FloatArray]:
+    """Split data into training and testing sets."""
+    N = len(y)
+    data_idx = list(range(N))
+    random.shuffle(data_idx)
+    break_idx = round(test_percent / 100 * N)
+    training_idx = data_idx[break_idx:]
+    testing_idx = data_idx[:break_idx]
+    X_train = X[training_idx, :]
+    y_train = y[training_idx]
+    X_test = X[testing_idx, :]
+    y_test = y[testing_idx]
+    return X_train, y_train, X_test, y_test
 
 
-# generate English document
-text = "Four score and seven years ago, our fathers brought forth, upon this continent, a new nation, conceived in liberty, and dedicated to the proposition that all men are created equal."
-text = (text + " ") * 100  # make the document very long
+def generate_data_token_counts(
+    h0_documents: list[list[str]], h1_documents: list[list[str]]
+) -> tuple[FloatArray, FloatArray, FloatArray, FloatArray]:
+    """Generate training and testing data with raw token counts."""
+    X: FloatArray = np.array(
+        [
+            sum_token_embeddings([onehot(vocabulary_map, token) for token in sentence])
+            for sentence in h0_documents
+        ]
+        + [
+            sum_token_embeddings([onehot(vocabulary_map, token) for token in sentence])
+            for sentence in h1_documents
+        ]
+    )
+    y: FloatArray = np.array(
+        [0 for sentence in h0_documents] + [1 for sentence in h1_documents]
+    )
+    return split_train_test(X, y)
+
+
+def build_vocabulary(h0_documents, h1_documents):
+    vocabulary = sorted(
+        set(token for sentence in austen + carroll for token in sentence)
+    ) + [None]
+    vocabulary_map = {token: idx for idx, token in enumerate(vocabulary)}
+    return vocabulary_map
+
+
+def train_naive_bayes(X_train, y_train):
+    h0_documents = X_train[y_train == 0]
+    h1_documents = X_train[y_train == 1]
+
+    ph0 = len(h0_documents) / len(y_train)
+    ph1 = len(h1_documents) / len(y_train)
+
+    counts0 = np.sum(h0_documents, axis=0) + 1
+    counts1 = np.sum(h1_documents, axis=0) + 1
+
+    p0 = counts0 / counts0.sum()
+    p1 = counts1 / counts1.sum()
+
+    return ph0, ph1, p0, p1
+
+
+def test_naive_bayes(X_test, y_test, ph0, ph1, p0, p1):
+    num_correct = 0
+    for i in range(len(y_test)):
+        sentence = X_test[i]
+        label = y_test[i]
+        h0_logp_unnormalized = sentence.T @ np.log(p0) + np.log(ph0)
+        h1_logp_unnormalized = sentence.T @ np.log(p1) + np.log(ph1)
+
+        # normalize
+        logp_data = np.logaddexp(h0_logp_unnormalized, h1_logp_unnormalized)
+        h0_logp = h0_logp_unnormalized - logp_data
+        h1_logp = h1_logp_unnormalized - logp_data
+
+        # make guess
+        pc0 = np.exp(h0_logp)
+        pc1 = np.exp(h1_logp)
+        guess = 1 if pc1 > pc0 else 0
+
+        if guess == label:
+            num_correct += 1
+
+    return num_correct / len(y_test)
+
 
 austen = nltk.corpus.gutenberg.sents("austen-sense.txt")
 carroll = nltk.corpus.gutenberg.sents("carroll-alice.txt")
 
+h0_documents = austen
+h1_documents = carroll
 
-# tokenize - split the document into a list of little strings
-tokens = [char for char in text]
+vocabulary_map = build_vocabulary(h0_documents, h1_documents)
+X_train, y_train, X_test, y_test = generate_data_token_counts(austen, carroll)
 
-# encode as {0, 1}
-vocabulary = [chr(i + ord("a")) for i in range(26)] + [" ", None]
 
-encodings = [onehot(vocabulary, token) for token in tokens]
+# Train Naive Bayes
+ph0, ph1, p0, p1 = train_naive_bayes(X_train, y_train)
 
-# train model
-model = UnigramModel()
-model.train(encodings)
 
-# compute probability
-test_data = "The quick brown fox jumps over the lazy dog."
-tokens = [char for char in test_data]
-encodings = [onehot(vocabulary, token) for token in tokens]
-log_p = model.apply(encodings)
+# Test Naive Bayes
+print("Naive Bayes (train):", test_naive_bayes(X_train, y_train, ph0, ph1, p0, p1))
+print("Naive Bayes (test):", test_naive_bayes(X_test, y_test, ph0, ph1, p0, p1))
 
-# print
-print(f"learned p value: {model.p}")
-print(f"log probability of document: {log_p}")
-print(f"probability of document: {math.exp(log_p)} (this is due to underflow)")
+# Create synthetic data:
+num_words_per_sentence = 10
+num_sentences = 100
